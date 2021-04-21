1df7fb36a798d30e2e71100989267c3944a34787
Chathura Madhusanka <66965842+chathuramadusanka@users.noreply.github.com>
2021-04-21 12:44:15 +0530

    updating models.py variables in kycInfo and kycinfo_temp

--- kyc_django/kyc_django/kyc/models.py
+++ kyc_django/kyc_django/kyc/models.py
@@ -54,9 +54,7 @@ class Kyc_Info(models.Model):
     office_num = models.CharField(max_length=20, blank=True, null=True)
     home_num = models.CharField(max_length=20, blank=True, null=True)
     email_add = models.CharField(max_length=50, blank=True, null=True)
-    
-    # secound page
-    occu_state = models.CharField(max_length=50, blank=True, null=True)
+
     
     
 
@@ -74,34 +72,61 @@ class Kyc_Info(models.Model):
         return self.full_name
 
 class Kyc_Infotemp(models.Model):
+    
+    salutation_temp = models.CharField(max_length=5)
     full_name_temp = models.CharField(max_length=200)
     name_init_temp = models.CharField(max_length=100)
+    profile_pic_temp = models.BinaryField(blank=True)
     id_type_temp = models.CharField(max_length=50)
     nics_no_temp = models.CharField(max_length=50)
+    date_of_birth_temp = models.DateField('%Y-%m-%d')
     driv_lic_temp = models.CharField(max_length=50, blank=True, null=True)
     driv_exp_temp = models.CharField(max_length=20, blank=True, null=True)
     pass_no_temp = models.CharField(max_length=50, blank=True, null=True)
     pass_exp_temp = models.CharField(max_length=20, blank=True, null=True)
+    birth_cernum_temp = models.CharField(max_length=10, blank=True, null=True)
+    post_id_temp = models.CharField(max_length=10, blank=True, null=True)
+    oafsc_temp = models.CharField(max_length=10, blank=True, null=True)
+    visa_copy_temp = models.BinaryField(blank=True)
+    othe_identity_doc_temp = models.BinaryField(blank=True)
     nationality_temp = models.CharField(max_length=50)
     nationality_other_temp = models.CharField(max_length=50, blank=True, null=True)
-    #date_of_birth_temp = models.DateField('%Y-%m-%d', blank=True, null=True)
-    date_of_birth_temp = models.CharField(max_length=20)
+    type_of_visa_temp = models.CharField(max_length=20,blank=True)
+    visa_exp_temp = models.CharField(max_length=20, blank=True, null=True)
+    other_types_temp = models.CharField(max_length=20,blank=True)
+    other_exp_temp = models.CharField(max_length=20, blank=True, null=True)
+    foreign_addre_temp = models.CharField(max_length=200, blank=True, null=True)
+    vari_doc_type_temp = models.CharField(max_length=200, blank=True, null=True)
+    vari_doc_temp = models.BinaryField(blank=True)
+    pep_person_temp = models.CharField(max_length=200, blank=True, null=True)
+    us_city_temp = models.CharField(max_length=200, blank=True, null=True)
+
+    # residential information
+    resident_sri_temp = models.CharField(max_length=10, blank=True, null=True)
+    country_resident_temp =models.CharField(blank=True, max_length=50)
+
+    # current Address
     house_no_temp = models.CharField(max_length=20)
     street_temp = models.CharField(max_length=20)
     city_temp = models.CharField(max_length=20)
-    #country_temp = models.CharField(max_length=20)
+    postal_code_temp = models.CharField(max_length=10, blank=True, null=True)
+    state_address_temp = models.CharField(max_length=20, blank=True, null=True)
+
+    # permenent Address
+    house_no_per_temp = models.CharField(max_length=20)
+    street_per_temp = models.CharField(max_length=20)
+    city_per_temp = models.CharField(max_length=20)
+    postal_code_per_temp = models.CharField(max_length=10, blank=True, null=True)
+    
+    # contact infromation
     mob_no_temp = models.CharField(max_length=20)
     office_num_temp = models.CharField(max_length=20, blank=True, null=True)
     home_num_temp = models.CharField(max_length=20, blank=True, null=True)
     email_add_temp = models.CharField(max_length=50, blank=True, null=True)
-    occu_state_temp = models.CharField(max_length=50)
     red_flag_temp = models.CharField(max_length=5, blank=True, null=True)
     blue_flagadd_temp = models.CharField(max_length=5, blank=True, null=True)
     blue_flag_temp = models.CharField(max_length=5, blank=True, null=True)
-    profile_pic = models.BinaryField(blank=True)
-    Birth_certificate_number = models.CharField(max_length=10, blank=True, null=True)
-    Postal_ID = models.CharField(max_length=10, blank=True, null=True)
-    OAFSC = models.CharField(max_length=10, blank=True, null=True)
+    
 
     class Meta:
         db_table = "Kyc_Infotemp"
