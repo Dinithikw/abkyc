c927cb31f160f69bd8f7b9391c437cee92b7ab63
Dinithikw <Dinithikw@users.noreply.github.com>
2021-05-02 22:50:26 +0530

    Update views.py

--- kyc_django/kyc_django/kyc/views.py
+++ kyc_django/kyc_django/kyc/views.py
@@ -879,8 +879,8 @@ def search_val(request):
 
         if Kyc_Info.objects.filter(nics_no_temp=nic_no).exists():
             messages.warning(request, 'containging kyc')
-            return render(request, 'kyc/index.html')
+            return render(request, 'kyc/search.html')
         else:
             messages.success(request, 'no kyc new one')
-    
+            return render(request, 'kyc/index.html')
     return render(request, 'kyc/search.html')
